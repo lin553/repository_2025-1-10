0a99b09834ee5c5184d39510b95b8fe4f12fe9aa
lin553 <share_pvijwi615@hotmail.com>
2025-01-19 09:33:50 +0800

    new file:   "try_from_feature-page_all/P13_\347\256\200\345\215\225\347\275\221\351\241\265\346\212\223\345\217\226\347\244\272\344\276\213_findAll\345\207\275\346\225\260.py"

---
+++ try_from_feature-page_all/P13_简单网页抓取示例_findAll函数.py
@@ -0,0 +1,31 @@
+from urllib.request import urlopen
+from urllib.error import HTTPError
+from bs4 import BeautifulSoup
+
+
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsobj = BeautifulSoup(html.read(),  features="html.parser") # 如果没有：features="html.parser"  ，vscode会报错：GuessedAtParserWarning：未明确指定解析器，因此我使用的是此系统的最佳可用 HTML 解析器 （“html.parser”）。
+                                                                        # 这通常不是问题，但如果在另一个系统或不同的虚拟环境中运行此代码，它可能会使用不同的解析器并表现不同。
+
+    except AttributeError as e:
+        return None
+    return bsobj
+
+# bsobj = getTitle('https://sina.com.cn')
+bsobj = getTitle('https://cn.bing.com/dict/search?q=color&FORM=BDVSP2&qpvt=color')
+if bsobj is None:
+    print("bsobj could not be found")
+else:
+    nameList = bsobj.findAll('span',{'class':'green'})      # 失败：现在网页没有直接写颜色了，都是找不到了，无论是green、yellow、red都没有。
+    print("namelist", nameList)
+
+    if nameList:
+        for name in nameList:
+            print(name.get_text())
+    else:
+        print("nameList could not be found")
