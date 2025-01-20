bbd507f3a5196ae3bcaf6bcf065484fc291877a4
lin553 <share_pvijwi615@hotmail.com>
2025-01-20 10:39:54 +0800

    modified:   "try_from_feature-page_all/P3_scrapetest_\347\256\200\345\215\225\346\212\223\345\217\226\347\275\221\351\241\265\347\244\272\344\276\213.py"

--- try_from_feature-page_all/P3_scrapetest_简单抓取网页示例.py
+++ try_from_feature-page_all/P3_scrapetest_简单抓取网页示例.py
@@ -4,5 +4,6 @@ from urllib.request import urlopen
 
 # html = urlopen('http://pythonscraping.com/pages/page1.html')      # 这个网页已经不能访问了
 # html = urlopen('https://www.amazon.com/-/zh_TW/')
-html = urlopen('http://www.bing.com')
+html = urlopen('https://finance.sina.com.cn/stock/')
+# html = urlopen('http://www.bing.com')
 print(html.read())
